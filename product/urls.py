40513480654af0839bb86e3d6c10199699f5aedb
sahinmehmetemre <msahin80@icloud.com>
2020-04-17 20:54:58 +0300

    product uygulaması ve category modeli eklendi

---
+++ product/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = {
+    # ex: /home/
+    path('', views.index, name='index'),
+    # ex: /home/2/
+    # path('<int:question_id>/', views.detail, name="detail"),
+}
